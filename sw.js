8b68b6289e80df4f3a20e67d63dcc5a1b998607d
Robert J Hayes <thebobsomest1@gmail.com>
2026-08-19 21:45:32 -0500

    fix: cache only successful responses and await writes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='grow-vault-v2';
+const CACHE='grow-vault-v3';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith((async()=>{try{const response=await fetch(event.request);if(response.ok){const copy=response.clone();event.waitUntil(caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{}))}return response}catch{return (await caches.match(event.request))||(await caches.match('./index.html'))}})())});
